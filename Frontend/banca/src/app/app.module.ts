c0b4f5802ad86020ee2493d5f226250373530a7c
Miguel Angel <miguelksangel@gmail.com>
2021-04-08 21:07:02 -0600

    201700543 - Pagina web iniciada

---
+++ Frontend/banca/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Frontend/banca/src/app/models/Task.ts
@@ -0,0 +1,13 @@
+export interface Usuario{
+    id_cuenta:Number;
+    nombres:String;
+    apellidos:String;
+    dpi:number;
+    saldo:number;
+    correo:String;
+    pass:String;
+}
+
+
+
+
