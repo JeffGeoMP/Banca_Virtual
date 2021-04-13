2c4ee7eb1be98f7a1e4e7dc16f571efadedf893c
Manases <2791808530307@ingenieria.usac.edu.gt>
2021-04-13 00:10:37 -0600

    201700529 - small bugfix

--- Frontend/banca/src/app/app-routing.module.ts
+++ Frontend/banca/src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-<<<<<<< HEAD
 import { ReportesComponent } from './components/reportes/reportes.component';
-=======
 import {ConsultaSaldoComponent} from './components/consulta-saldo/consulta-saldo.component'
->>>>>>> develop
 
 const routes: Routes = [
   {
     path: 'Login',
     component: LoginComponent
-<<<<<<< HEAD
   },
   { path: 'reportes', component: ReportesComponent},
-=======
-  },{ path:'Consulta', component: ConsultaSaldoComponent},
->>>>>>> develop
+  { path:'Consulta', component: ConsultaSaldoComponent},
   {
     path: '**',
     pathMatch: 'full',
