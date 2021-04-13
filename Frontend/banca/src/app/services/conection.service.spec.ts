22b6b5d2d92129a0588991c8824f6cecd1c8cbec
Miguel Angel <miguelksangel@gmail.com>
2021-04-12 21:35:31 -0600

    Conflicts solved 2

--- Frontend/banca/src/app/services/conection.service.spec.ts
+++ Frontend/banca/src/app/services/conection.service.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ConectionService } from './conection.service';
 import { Usuario } from '../models/Task';
