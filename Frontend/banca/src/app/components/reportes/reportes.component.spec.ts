9c667b8a42c9ccb9eab5147fee936d205a29d033
Manases <2791808530307@ingenieria.usac.edu.gt>
2021-04-12 23:38:42 -0600

    201700529 - Pruebas unitarias

--- Frontend/banca/src/app/components/reportes/reportes.component.spec.ts
+++ Frontend/banca/src/app/components/reportes/reportes.component.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { ReportesComponent } from './reportes.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Usuario } from 'src/app/models/Task';
-//import { ConectionService } from 'src/app/services/conection.service';
-class conectionServiceMock {
-  //getItem = jasmine.createSpy('conectionService.getTransacciones');
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Transaccion, Usuario } from 'src/app/models/Task';
+import { ConectionService } from 'src/app/services/conection.service';
+import { of } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+class mockReporte extends ConectionService {
+  getTransacciones(id:Number){
+    console.log("Mock Reporte");
+    let data: Transaccion[] = [
+      {
+        id_transferencia: 2,
+        fecha: "2021-04-11",
+        monto: 1.50,
+        nombres: "Pablo",
+        apellidos: "Rodriguez", 
+      },
+    ];
+    return of(data);
+  }
 }
 
 fdescribe('ReportesComponent', () => {
-  let component: ReportesComponent;
   let fixture: ComponentFixture<ReportesComponent>;
-  //let service: ConectionService;
+  let component: ReportesComponent;
+  let service: ConectionService;
+  let mockService: mockReporte;
+  let http: HttpClient;
+
   let user: Usuario = {
     id_cuenta:3,
     nombres:"Daniel",
@@ -27,15 +44,10 @@ fdescribe('ReportesComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ ReportesComponent ],
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        HttpClientModule
       ],
-      providers: [
-        /*ConectionService,
-        {
-          provide: ConectionService,
-          useClass: conectionServiceMock,
-        }*/
-      ]
+      providers: [ConectionService, HttpClient],
     })
     .compileComponents();
   });
@@ -44,6 +56,27 @@ fdescribe('ReportesComponent', () => {
     fixture = TestBed.createComponent(ReportesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    service = TestBed.get(ConectionService);
+    mockService = new mockReporte(http);
+    
+    component.transacciones = [
+      {
+        id_transferencia: 1,
+        fecha: "2021-04/11",
+        monto: 12.10,
+        nombres: "Marta",
+        apellidos: "Flores",
+      },
+      {
+        id_transferencia: 2,
+        fecha: "2021-04/11",
+        monto: 25.15,
+        nombres: "Marta",
+        apellidos: "Flores",
+      },
+    ];
+    
   });
 
   afterEach(()=>{
@@ -71,35 +104,36 @@ fdescribe('ReportesComponent', () => {
   });
 
   it('Obtener datos', ()=>{
+    component = new ReportesComponent(mockService);
     component.transacciones = [];
+    component.id_usuario = user.id_cuenta;
+    let resultado;
 
-    component.obtener_transacciones();
-
-    expect(component.transacciones.length).toBeLessThanOrEqual(0);
-
-    //expect().toBeUndefined();
+    service.getTransacciones(user.id_cuenta).subscribe(data=>{
+      resultado = data;
+      console.log(resultado);
+      expect(component.transacciones.length).toBeGreaterThanOrEqual(0);
+    });
+    expect(component.obtener_transacciones()).toBeUndefined();
   });
 
   it('Descargar pdf', ()=>{
-    component.transacciones = [
-      {
-        id_transferencia: 1,
-        fecha: "2021-04/11",
-        monto: 12.10,
-        nombres: "Marta",
-        apellidos: "Flores",
-      },
-      {
-        id_transferencia: 2,
-        fecha: "2021-04/11",
-        monto: 25.15,
-        nombres: "Marta",
-        apellidos: "Flores",
-      },
-    ];
-
     component.ngOnInit();
 
     expect(component.downloadPDF()).toEqual("pdf descargado");
   });
+
+  /**
+   fit('Error datos', inject([HttpTestingController, ConectionService],
+    (httpMock: HttpTestingController, service: ConectionService) => {
+      service.getTransacciones(1).subscribe(data => {
+        console.log("tood ok");
+      });
+
+      const req = httpMock.expectOne(`http://localhost:3000/transacciones/${user.id_cuenta}`);
+      expect(req.request.method).toEqual('GET');
+      req.flush({ "pass": "1234", "id_cuenta": -1 });
+    })
+  );
+   */
 });
