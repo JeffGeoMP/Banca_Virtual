6aa0f1452347791feca505ab718dc150824a0caa
Hayrton <2172182021503@ingenieria.usac.edu.gt>
2021-04-21 21:54:35 -0600

    201313875 - Pruebas unitarias para transferencia

--- Frontend/banca/src/app/components/transferencia/transferencia.component.spec.ts
+++ Frontend/banca/src/app/components/transferencia/transferencia.component.spec.ts
@@ -4,8 +4,15 @@ import { TransferenciaComponent } from './transferencia.component';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 import { ConectionService } from 'src/app/services/conection.service';
 import { Usuario, Transaccion } from 'src/app/models/Task';
-import { of } from 'rxjs';
+import { observable, of } from 'rxjs';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Mock } from 'protractor/built/driverProviders';
+
+class HttpClientMock{
+  get = jasmine.createSpy('httpClient.get');
+  post = jasmine.createSpy('httpClient.post');
+}
+
 
 class mockTransaccion extends ConectionService{
   transferencia(data){
@@ -20,6 +27,7 @@ fdescribe('TransferenciaComponent', () => {
   let service: ConectionService;
   let mockService: mockTransaccion;
   let http: HttpClient;
+  let httpClientMock: HttpClientMock;
 
 
   let user: Usuario = {
@@ -69,8 +77,42 @@ fdescribe('TransferenciaComponent', () => {
     expect(component.nombreemisor).toEqual(user.nombres);
   });
 
+  
+  it('Transferencia exitosa',()=>{
+    let data = {
+      'fecha': '00/00/0000',
+      'id_emisor':1,
+      'id_receptor':1,
+      'monto':10
+    }
 
-  it('Transferencia exitosa',
+    expect(component.transfer()).toBeUndefined();
+
+     service.transferencia(data).subscribe((res)=>{
+      expect(httpClientMock.post).toHaveBeenCalledWith(`http://localhost:3000/transferencia`);
+      expect(res).toBeTruthy({"msg":"true"});
+      expect(alert).toHaveBeenCalledWith("Transferencia exitosa")
+     });
+  });
+
+  it('Transferencia no exitosa',()=>{
+    let data = {
+      'fecha': 0,
+      'id_emisor':1,
+      'id_receptor':1,
+      'monto':10
+    }
+
+    expect(component.transfer()).toBeUndefined();
+
+     service.transferencia(data).subscribe((res)=>{
+      expect(httpClientMock.post).toHaveBeenCalledWith(`http://localhost:3000/transferencia`);
+      expect(res).toBeFalsy(null);
+      expect(alert).toHaveBeenCalledWith("Transferencia fallida");
+     });
+  });
+
+  /*it('Transferencia exitosa',
     inject([HttpTestingController, ConectionService],
       (httpMock: HttpTestingController, service: ConectionService) => {
         // We call the service
@@ -87,10 +129,10 @@ fdescribe('TransferenciaComponent', () => {
         const req = httpMock.expectOne('http://localhost:3000/transferencia');
         expect(req.request.method).toEqual('POST');
         req.flush({ "msg":"true" });
-
-        
       })
-  );
+  );*/
+
+
 
 
 });
